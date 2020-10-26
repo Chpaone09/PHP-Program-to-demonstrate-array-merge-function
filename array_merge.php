788cad4fdc443fc7767b83bb9917151592586fde
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-26 22:24:49 +0530

    Program to demonstrate array_merge function in PHP

---
+++ array_merge.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+***********************************************************
+
+Topic   : Program to demonstrate array_merge function in PHP.
+Author  : Chpaone09®
+Date    : Oct 26, 2020
+
+************************************************************
+*/
+
+
+$a = array(1,2,3,4);
+$b = array(5,6,7,8);
+
+$a1 = array_merge($a, $b);
+echo "<pre>";print_r($a1);
+
+?>
